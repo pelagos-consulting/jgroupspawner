552ce56523d9a89498282c192bb82309d09dc2ad
drtpotter <tobympotter@gmail.com>
2023-10-11 16:10:07 +1100

    latest

--- jladgroupspawner/jladgroupspawner.py
+++ jladgroupspawner/jladgroupspawner.py
@@ -12,27 +12,25 @@ def set_user_setuid(username, extra_groups):
     home = user.pw_dir
     gids = [gid]
 
+    try:
+        # Get all GID's for a user, put them into a set
+        gids=set(os.getgrouplist(username, gid))
+    except Exception as e:
+        print(f'Could not use os.getgrouplist to get groups for {username}, error is: {repr(e)}', file=sys.stderr)
 
     # Try to add the extra groups if they are passed in
-    extra_gids=[]
     for gname in extra_groups:
         try:
-            extra_gids.append(grp.getgrnam(gname).gr_gid)
-        except AttributeError:
-            pass
-
-    try:
-        # Get all gid's for a user
-        gids=os.getgrouplist(user, gid) + extra_gids
-        print('Set gids for %s to %s' % (username, gids), file=sys.stderr)
-    except Exception as e:
-        print('Could not get groups for user: %s' % username, file=sys.stderr)
+            # Try to add the extra groups to the set
+            gids.add(grp.getgrnam(gname).gr_gid)
+        except Exception as e:
+            print(f'Error adding extra group {gname} to the set of groups, error is: {repr(e)}', file=sys.stderr)
 
     def preexec():
         os.setgid(gid)
         try:
-            os.setgroups(gids)
-            print('User = %s, Gid = %s, gids = %s' % (username, gid, gids), file=sys.stderr)
+            os.setgroups(list(gids))
+            print('Set groups for user = %s, Gid = %s, gids = %s' % (username, gid, gids), file=sys.stderr)
         except Exception as e:
             print('Failed to set groups for user %s' % username, file=sys.stderr)
 
@@ -43,9 +41,12 @@ def set_user_setuid(username, extra_groups):
     return preexec
 
 class JLADGroupSpawner(LocalProcessSpawner):
-    # Define 
-    # c.JLADGroupSpawner.extra_groups = ["this_group", "that_group"] 
-    # in Jupyterhub config
+    # Define a hook function in jupyterhub_config.py in order to set extra_groups
+    # like this
+    # def hook_fun(spawner):
+    #     spawner.extra_groups=["video", "render"]
+    # c.Spawner.pre_spawn_hook = hook_fun
+
     extra_groups=[]
     
     def make_preexec_fn(self, name):
